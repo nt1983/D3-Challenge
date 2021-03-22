699c1d8b62932f8af7690b75430e1439e647933c
nt1983 <73156205+nt1983@users.noreply.github.com>
2021-03-21 21:22:24 -0700

    Update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -24,4 +24,61 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Initial Params
-var chosenXAxis = "In Poverty (%)";
+var chosenXAxis = "poverty";
+var chosenYAxis="healthcare";
+var xLabel = ["poverty", "age", "income"];   
+var yLabel = ["obesity", "smokes", "healthcare"];
+
+// function used for updating x-scale var upon click on axis label
+function xScale(weightdata, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(weightdata, d => d[chosenXAxis]) * 0.8,
+      d3.max(weightdata, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+  return xLinearScale;
+}
+
+// function used for updating xAxis var upon click on axis label
+function renderXAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+  return xAxis;
+}
+
+// function used for updating yAxis var upon click on axis label
+function renderYAxes(newYScale, yAxis) {
+  var leftAxis = d3.axisLeft(newYScale);
+  yAxis.transition()
+    .duration(1000)
+    .call(leftAxis);
+  return yAxis;
+}
+
+// function used for updating circles group with a transition to new circles
+function renderCircles(circlesGroup, newXScale, newYScale, chosenXAxis, chosenYAxis) {
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]))
+    .attr("cy", d => newYScale(d[chosenYAxis]));
+  return circlesGroup;
+}
+
+// Function used for updating text in circles group with a transition to new text.
+function renderText(circletextGroup, newXScale, newYScale, chosenXAxis, chosenYAxis) {
+  circletextGroup.transition()
+    .duration(1000)
+    .attr("x", d => newXScale(d[chosenXAxis]))
+    .attr("y", d => newYScale(d[chosenYAxis])); 
+  return circletextGroup;
+}
+
+
+
+
+
+
+
